772e22ca092087283e4163bd3558048a8c1df88c
zak258 <zakaria.k.hamdan@gmail.com>
2020-06-17 11:02:53 +0200

    add deploy script

---
+++ build/precache-manifest.aece12a89f50f2958d18570a898ce2c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5183fe1b44954696894f3f8575d721a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efe1b56a4fefc1cc9da2",
+    "url": "/static/css/main.910f42ee.chunk.css"
+  },
+  {
+    "revision": "721adb0563dad1169c45",
+    "url": "/static/js/2.c667716f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c667716f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe1b56a4fefc1cc9da2",
+    "url": "/static/js/main.d4c6604d.chunk.js"
+  },
+  {
+    "revision": "25d3e7f4c7dd4b469149",
+    "url": "/static/js/runtime-main.ccab44b3.js"
+  }
+]);
