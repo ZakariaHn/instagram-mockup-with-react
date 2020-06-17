d3c3243af1d4250077cd93eef26b00bab1be85f1
zak258 <zakaria.k.hamdan@gmail.com>
2020-06-17 14:53:21 +0200

    Updates

---
+++ precache-manifest.75a96021d823ca09057914bf1de46f3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440e2df1cd037450de1eaaa04efde8b8",
+    "url": "/instagram-mockup-with-react/index.html"
+  },
+  {
+    "revision": "efe1b56a4fefc1cc9da2",
+    "url": "/instagram-mockup-with-react/static/css/main.910f42ee.chunk.css"
+  },
+  {
+    "revision": "721adb0563dad1169c45",
+    "url": "/instagram-mockup-with-react/static/js/2.c667716f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/instagram-mockup-with-react/static/js/2.c667716f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe1b56a4fefc1cc9da2",
+    "url": "/instagram-mockup-with-react/static/js/main.d4c6604d.chunk.js"
+  },
+  {
+    "revision": "c0e720a37fccbcd4e023",
+    "url": "/instagram-mockup-with-react/static/js/runtime-main.50aec0b9.js"
+  }
+]);
